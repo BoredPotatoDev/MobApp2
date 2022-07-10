550487c77caf7e42a0342154f15095bb70bcba86
BoredPotatoDev <martinjoshuaddetona@iskolarngbayan.pup.edu.ph>
2022-07-10 23:50:35 +0800

    sepuku

--- register.class.php
+++ register.class.php
@@ -53,13 +53,14 @@ class RegisterUser{
         return false;
     }
 
-    private function insertUser(){
-        if($this->udernameExist() == FALSE){
-            array_push($this->stored_users, $this->new_user);
-            if(file_put_contents($this->storage, json_encode($this->stored_users, JSON_PRETTY_PRINT))){
-                return $this->success = "You have been added to the database";
-            }else
-                return $this->success = "Something went wrong";
-        }
-    }
+	private function insertUser(){
+		if($this->usernameExists() == FALSE){
+			array_push($this->stored_users, $this->new_user);
+			if(file_put_contents($this->storage, json_encode($this->stored_users, JSON_PRETTY_PRINT))){
+				return $this->success = "Your registration was successful";
+			}else{
+				return $this->error = "Something went wrong, please try again";
+			}
+		}
+	}
 }
