85df84c9d691a7ed066b0336e0989a2015c4dcd6
DrSeuss127 <clintcalumpad@gmail.com>
2022-07-11 01:01:35 +0800

    Update login.class.php

--- login.class.php
+++ login.class.php
@@ -11,9 +11,10 @@ class LoginUser{
 
 
     // class methods
-    public function __construct($FamilyName, $StudentNumber){
+    public function __construct($FamilyName, $StudentNumber, $DateofBirth){
         $this->FamilyName = $FamilyName;
-        $this->password = $StudentNumber;
+        $this->StudentNumber = $StudentNumber;
+        $this->DateofBirth = $DateofBirth;
         $this->stored_users = json_decode(file_get_contents($this->storage), true);
         $this->login();
     }
@@ -21,7 +22,7 @@ class LoginUser{
     private function login(){
         foreach($this->stored_users as $user) {
             if($user['FamilyName'] == $this->FamilyName){
-                if(password_verify($this->StudentNumber, $user['password'])){
+                if(password_verify($this->StudentNumber, $user['StudentNumber'])){
                     session_start();
                     $_SESSION['user'] = $this->FamilyName;
                     header("location: account.php"); exit();
