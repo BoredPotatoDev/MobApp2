5d456ed3bd6898dd5c4f6d86a3d3c3421eb59129
BoredPotatoDev <martinjoshuaddetona@iskolarngbayan.pup.edu.ph>
2022-07-10 22:33:02 +0800

    index updated

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php require("register.class.php")?>
+
+<?php
+    if(isset($_POST['submit'])){
+        $user = new RegisterUser($_POST['FN'], $_POST['SN'], $_POST['DoB']);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device.width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Register form</title>
+</head>
+<body>
+
+    <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
+        <h2>Register form</h2>
+        <h4>All fields are <span>required</span></h4>
+
+        <label>Family Name</label>
+        <input type="text" name="FN">
+
+        <label>Student Number</label>
+        <input type="number" name="SN">
+
+        <label>Date of Birth</label>
+        <input type="date" name="DoB">
+
+        <button type="submit" name="submit">Register</button>
+
+        <p class="error"><?php echo @user->error ?></p>
+        <p class="success"><?php echo @user->success ?></p>
+</body>
+</html>
---
+++ register.class.php
@@ -0,0 +1,64 @@
+<?php
+
+class RegisterUser{
+    private $FN;
+    private $SN;
+    private $DoB;
+    public $error:
+    public $success;
+    private $storage = "login.json";
+    private $stored_users;
+    private $new_user;
+
+    public function __construct($FN, $SN, $DoB){
+        $this->FN = trim(this->FN);
+        $this->username = filter_var($FN, FILTER_SANITIZED_STRING);
+
+        $this->SN = trim(this->FN);
+        $this->STDNTnumber = filter_var($SN, FILTER_SANITIZED_STRING);
+
+        $this->DoB = trim(this->FN);
+        $this->date = filter_var($DoB, FILTER_SANITIZED_STRING);
+
+        $this->stored_users = json_decode(file_get_contents($this->storage),true);
+
+        $this->new_user = [
+            "username" => $this->FN,
+            "STDNTnumber" => $this->SN,
+            "date" => $this->DoB,
+        ]
+
+        if($this->checkFieldValues()){
+            $this->insertUser();
+        }
+    }
+
+    private function checkFieldValues(){
+        if(empty($this->username) || empty($this->STDNTnumber) || empty($this->date)){
+            $this->error = "ALL FIELDS ARE REQUIRED"
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private function usernameExist(){
+        foreach($this->stored_users as $user){
+            if($this->username == $user['username']){
+                $this->error = "You Already Have an Account!";
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function insertUser(){
+        if($this->udernameExist() == FALSE){
+            array_push($this->stored_users, $this->new_user);
+            if(file_put_contents($this->storage, json_encode($this->stored_users, JSON_PRETTY_PRINT))){
+                return $this->success = "You have been added to the database"
+            }else
+                return $this->success = "Something went wrong"
+        }
+    }
+}
